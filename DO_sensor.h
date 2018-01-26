683b2a505efcdf9c8800389b9a7dd528afc86aee
Tran Michelle <tran.michel02@stvrain.k12.co.us>
2018-01-25 19:51:33 -0700

    Header file for dissolved oxygen sensor

---
+++ DO_sensor.h
@@ -0,0 +1,32 @@
+/* 
+Header File for Dissolved Oxygen Sensor
+Project: Innovation Center's Project Poseidon
+Created: 1/25/18
+*/
+
+// Define DO sensor address
+#define DO_sensor_address 0x61
+
+// DO sensor commands
+// Initialization
+#define DO_factory_reset "Factory"
+#define DO_read_device_info	"i"
+#define DO_remove_param "O"
+#define DO_save_cal "Export"
+#define DO_load_cal "Import"
+#define DO_toggle_LED "L"
+#define DO_status "Status"
+// Calibration
+#define DO_cal "Cal"
+#define DO_pressure "P"
+#define DO_salinity "S"
+#define DO_temp "T"
+// Measurement
+#define DO_return_read "R"
+#define DO_sleep "Sleep"
+
+// Function Prototypes
+void DO_calibration(void);
+uint8_t DO_initialization(void);
+uint8_t DO_compensation(void);
+uint8_t DO_read(void);
